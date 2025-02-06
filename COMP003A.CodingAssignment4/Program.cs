d227d3bc1cfa5de669b63ee7b12010de3b2c100d
MichaelBaker-boop <michaelbaker4@my.whccd.edu>
2025-02-05 18:47:15 -0800

    NewAssignment

--- COMP003A.CodingAssignment4/Program.cs
+++ COMP003A.CodingAssignment4/Program.cs
@@ -1,17 +1,43 @@
 ﻿// Author: Michael Baker
 // Course: COMP-003A
 // Faculty: Jonathan Cruz
-// Purpose: Inventory management application with a minimum of 10 elements in the collection.
+// Purpose: Inventory managament application with a minimum of 10 elements in the collection.
+
 namespace COMP003A.CodingAssignment4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int listingType;
-            Console.WriteLine("Welcome to the Inventory Management Application.\n");
-            Console.Write("Would you like to use arrays or lists (1 or 2)?");
-            listingType = int.Parse(Console.ReadLine());
+
+            int menuSelection;
+            new List<string>();
+            new List<int>();
+
+            bool menu = true;
+            while (menu)
+            {
+                // Create a menu and ask for choice from user
+                Console.WriteLine("Menu: ");
+                Console.WriteLine("1) Add a Product.");
+                Console.WriteLine("2) Update Product Quantity.");
+                Console.WriteLine("3) View Inventory Summary.");
+                Console.WriteLine("4) Exit.");
+
+                try
+                {
+                    menuSelection = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.Write("Invalid Input. Please enter a valid number.");
+                    throw;
+                }
+
+
+            }
+
+
         }
     }
 }
